05ff90ef318e18229d691ff43b68cb3a6fffdf89
nathaliapavan <nanahpavan@gmail.com>
2023-07-28 08:59:07 -0300

    feature: add configmap with volume

--- server.go
+++ server.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/configmap", configMap)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -15,4 +18,12 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func configMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "Hello, %s.", string(data))
 }
